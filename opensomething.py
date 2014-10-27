19e58cb2aeeba7783d7746efc90b2beb0d46559f
Rutger van Haasteren <vhaasteren@gmail.com>
2014-10-27 13:13:10 -0700

    opensomething was not committed

--- opensomething.py
+++ opensomething.py
@@ -25,4 +25,38 @@ from matplotlib.figure import Figure
 import numpy as np
 
 
+class OpenSomethingWidget(QtGui.QWidget):
+    """
+    The open-something Widget. First shown in the main window, if it is not
+    started with a command to open any file to begin with. This way, we don't
+    have to show an empty graph
+    """
+    def __init__(self, parent=None, openFile=None, **kwargs):
+        super(OpenSomethingWidget, self).__init__(parent, **kwargs)
+
+        self.parent = parent
+        self.openFileFn = openFile
+
+        self.initOSWidget()
+
+    def initOSWidget(self):
+        """
+        Initialise the widget with a button and a label
+        """
+        self.vbox = QtGui.QVBoxLayout()
+
+        button = QtGui.QPushButton("Open a file...")
+        button.clicked.connect(self.openFile)
+        self.vbox.addWidget(button)
+
+        self.setLayout(self.vbox)
+
+    def openFile(self):
+        """
+        Display the open file dialog, and send the parent window the order to
+        open the file
+        """
+        if not self.openFileFn is None:
+            filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '~/')
+            self.openFileFn(filename)
 
